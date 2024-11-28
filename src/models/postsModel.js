027086f7bd22eeda6548fa5928cbcd3c0483ea6b
amanmdest <amanmdest@gmail.com>
2024-11-28 10:27:36 -0300

    feat: post endpoint with images upload and file handling for photoblog posts

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,8 +1,14 @@
-import conectarAoBanco from '../config/dbConfig.js';
-const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
+import dbConnect from '../config/dbConfig.js';
+const conexao = await dbConnect(process.env.STRING_CONEXAO)
 
-export async function getTodosPosts() {
+export async function getAllPosts() {
     const db = conexao.db("imersao-instabytes")
     const colecao = db.collection("posts")
     return colecao.find().toArray()
+}
+
+export async function modelPost(newPost) {
+    const db = conexao.db("imersao-instabytes");
+    const colecao = db.collection("posts");
+    return colecao.insertOne(newPost);
 }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,9 +1,23 @@
 import express from 'express'
-import { listarPosts } from '../controllers/postsController.js ';
+import multer from 'multer'
+import { createPosts, listPosts, uploadImage } from '../controllers/postsController.js ';
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({ dest: "./uploads" , storage})
 
 const routes = (app) => {
     app.use(express.json());
-    app.get("/posts", listarPosts);
+    app.get("/posts", listPosts);
+    app.post("/posts", createPosts);
+    app.post("/upload", upload.single("image"), uploadImage);
 }
 
 export default routes;
